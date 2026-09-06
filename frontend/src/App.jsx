68c10e53b09c3dcdf292561a7e233573d890fe17
romarrizo054-tech <romarrizo054@gmail.com>
2026-09-06 17:43:17 -0300

    negocio.com

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
           {/* Aquí conectamos la ruta con el componente que acabamos de crear */}
           <Route path="/inventario" element={<Inventario />} />
           <Route path="/caja" element={<Caja />} />
-          <Route path="/venta" element={<Ventas />} />
+          <Route path="/ventas" element={<Ventas />} />
         </Routes>
       </div>
     </Router>
